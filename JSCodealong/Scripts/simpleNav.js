4b2d20a933cd440e35c76a043fc3bdcec30f8d33
ecknomichiel <michiel@frostvikenit.eu>
2017-06-15 16:26:19 +0200

    halfway simple menu

---
+++ JSCodealong/Scripts/simpleNav.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    var simpleNavController = function ($scope) {
+        $scope.section = "index";
+        $scope.onMenuClick = function (sectionName) {
+            $scope.section = sectionName;
+        }
+    };
+    mod = angular.module("ecknoModule");
+    mod.controller("simpleNavController", ["$scope", simpleNavController]);
+}());
